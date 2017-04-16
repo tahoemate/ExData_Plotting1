f7a04179e3098c85c99b0c6de358c1f33947cb63
Brian Hale <bhale@dentrek.com>
2017-04-16 14:29:10 -0500

    Plot 3 finished

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 # Class Exploratory Data Analysis - Week1
-# Plot 2: Plot of Global Active Power
+# Plot 3: Plot of 3 Sub Metering Vectors
 # Date Range:  2/1/2007 = 2/2/2007
 
 # Step 0.  
@@ -28,9 +28,13 @@ alldata <- read.csv(datafile,header=TRUE,sep=";",na.strings=c("?"),as.is=TRUE)
 data <- subset(alldata, as.Date(Date,"%d/%m/%Y") >= as.Date("2007-02-01") & as.Date(Date,"%d/%m/%Y") <= as.Date("2007-02-02") )
 
 # Plot Global Active Power vs Date/Time.  Remember to use type="l" or you'll get a scatterplot.
-plot( x=as.POSIXct(paste(strptime(as.Date(data$Date,"%d/%m/%Y"), "%Y-%m-%d"),data$Time)),y=data$Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="l")
+plot( x=as.POSIXct(paste(strptime(as.Date(data$Date,"%d/%m/%Y"), "%Y-%m-%d"),data$Time)),y=data$Sub_metering_1,ylab="Global Active Power (kilowatts)",xlab="",type="l")
+lines( x=as.POSIXct(paste(strptime(as.Date(data$Date,"%d/%m/%Y"), "%Y-%m-%d"),data$Time)),y=data$Sub_metering_2,type="l",col="red")
+lines( x=as.POSIXct(paste(strptime(as.Date(data$Date,"%d/%m/%Y"), "%Y-%m-%d"),data$Time)),y=data$Sub_metering_3,type="l",col="blue")
 
-dev.copy(png, "plot2.png")
+legend("topright", c("Sub_meeting_1","Sub_meeting_2","Sub_meeting_3"),lwd=1,col=c("black","red","blue"))
+
+dev.copy(png, "plot3.png")
 dev.off()
 
-print("Done Plot 2.")
+print("Done Plot 3.")
